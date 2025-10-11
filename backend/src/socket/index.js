f4afa381bb16b2e68f5da2e18ba040edec7667ff
Dogukan I. <dogukanincesu@gmail.com>
2025-10-11 05:12:59 +0300

    Add authentication middleware for socket connection

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -21,6 +21,22 @@ function initializeSocket(server, { app } = {}) {
     },
   })
 
+  // Authentication middleware
+  ioInstance.use((socket, next) => {
+    const { apiKey } = socket.handshake.auth
+    const expectedApiKey = process.env.API_KEY
+
+    if (expectedApiKey && apiKey !== expectedApiKey) {
+      // eslint-disable-next-line no-console
+      console.log(`[socket.io] Authentication failed for socket ${socket.id}`)
+      return next(new Error('Authentication failed'))
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(`[socket.io] Authentication successful for socket ${socket.id}`)
+    next()
+  })
+
   ioInstance.on('connection', (socket) => {
     // eslint-disable-next-line no-console
     console.log(`[socket.io] Client connected: ${socket.id}`)
