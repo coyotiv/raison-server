9405c593f1efd51eedeaa7d7a13021936a89227f
Ben Sukstorf <bsukstorf@gmail.com>
2025-10-11 02:18:35 +0200

    :bug: Fix layouts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,7 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { RouterView, useRoute } from 'vue-router'
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
-import AuthLayout from '@/layouts/AuthLayout.vue'
-
-const route = useRoute()
-const layouts = { default: DefaultLayout, auth: AuthLayout }
-const layout = computed(() => layouts[(route.meta.layout as 'auth' | 'default') || 'default'])
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <component :is="layout">
-    <RouterView />
-  </component>
+  <RouterView />
 </template>
